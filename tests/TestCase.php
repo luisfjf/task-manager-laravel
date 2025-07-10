283f7ad37efc88e226d9427faa13e0b1cbf5a2c5
Luis Jimenez <fernandojf90@gmail.com>
2025-07-09 21:33:36 -0600

    CI: change manifest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Vite; // <-- 1. Importa el Facade de Vite
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    /**
+     * Configura el entorno de prueba.
+     */
+    protected function setUp(): void // <-- 2. Añade este método
+    {
+        parent::setUp();
+
+        Vite::withoutManifest(); // <-- 3. Esta es la línea clave
+    }
 }
